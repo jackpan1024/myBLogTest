660f224198c5a427ec4ffc6ec41535d340b977bb
jackpan <jackpan7796@qq.com>
2018-11-06 20:03:57 +0800

    完善了注册功能和完成了登录的功能

--- blog/app.js
+++ blog/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const moment = require('moment')
 const mysql = require('mysql')
 
 const bodyparser = require('body-parser') //调用包 快速的获取客户端传来的数据
@@ -45,17 +46,39 @@ app.post('/register',(req,res)=>{
     conn.query(sql1,str.username,(err,result)=>{
         if(err) return res.send({status:400,msg:'查询失败'}) 
         if(result[0].count !==0) return res.send({status:400,msg:'用户名重复'})
+            str.ctime = moment().format('YY-MM-DD HH:mm:ss') // 服务器添加时间属性项
 
         const sql2 = 'insert into user set ?'
         conn.query(sql2,str,(err,result)=>{
+            // console.log(result)
             if(err) return res.send({status:400,msg:'插入数据失败'})
-            res.send({status:400,msg:'注册成功'})
+            if(result.affectedRows !==1 ) return res.send({status:400,msg:'注册失败'})
+            res.send({status:200,msg:'注册成功'})
         })
         
 
     })
 
    
+})
+
+//创建登录的post 连接
+app.post('/login',(req,res)=>{
+     const str = req.body
+    if(str.password.trim().length < 1 || str.username.trim().length < 1){
+        return res.send({status:400,msg:'用户名或者密码不能为空'})
+    }
+   
+    // console.log(str)
+    const sql = 'select * from user where username = ? and password = ?'
+    conn.query(sql,[str.username,str.password],(err,result)=>{
+        if(err) return  res.send({status:400,msg:'请求失败，请重新登录'})
+        //console.log(result) 加入数据没有匹配成功，那么返回的就是一个空的数组 说明resul就是一个数组
+        if (result.length == 0 ) return res.send({status:400,msg:'用户名或者密码不匹配！请重新登录'})
+        res.send({status:200,msg:'登录成功'})
+    })
+
+    
 })
 
 app.listen(3000,()=>{
